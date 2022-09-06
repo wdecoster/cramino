6e91f46fd72f36e1d7dea9354249ad788453a812
wdecoster <decosterwouter@gmail.com>
2022-09-06 22:15:47 +0200

    get checksum of file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use clap::AppSettings::DeriveDisplayOrder;
 use clap::{Parser, Subcommand};
-use log::info;
+use log::{error, info};
 //use rayon::prelude::*;
-use rand::Rng;
+use checksums::hash_file;
+use std::path::Path;
 
 pub mod calculations;
 pub mod extract_from_bam;
@@ -53,25 +54,31 @@ fn main() {
 }
 
 ///extract metrics from a bam file by subsampling reads
-fn extract_metrics(bamp: String, fraction: f32, threads: usize) {
+fn extract_metrics(bamp: String, fraction: f32, _threads: usize) {
     // the fraction determines how many reads to skip every time by inverting it
+    // every "inverse"th read is used
     let inverse = (1.0 / fraction) as usize;
-    // a random offset is chosen to make sure that every time the program is executed another set of reads are chosen
-    let offset = rand::thread_rng().gen_range(0..inverse);
-    // every "every"th read is used
-    let every = offset + inverse;
-    let (mut lengths, mut pids): (Vec<u32>, Vec<u32>) = extract_from_bam::extract(&bamp, every);
+    let (mut lengths, mut pids): (Vec<u32>, Vec<f32>) = extract_from_bam::extract(&bamp, inverse);
     // to get the yield, sum all lengths of the selected reads and multiply that again by the inverse of the fraction
+    if lengths.len() < 2 {
+        error!("Not enough reads to calculate metrics!");
+        panic!();
+    }
     lengths.sort_unstable();
-    pids.sort_unstable();
+    pids.sort_unstable_by(|a, b| a.partial_cmp(b).unwrap());
     let data_yield: u32 = lengths.iter().sum::<u32>() * inverse as u32;
     let numreads = lengths.len() * inverse;
     let n50 = calculations::get_n50(&lengths, data_yield);
     let median_length = calculations::median(&lengths);
     let median_pid = calculations::median(&pids);
     // get histograms for pid and length
-    println!("bam\tnumber_of_reads\tyield\tn50\tmedian_length\tmedian_pid\thistogram_length\thistogram_pid");
-    println!("{bamp}\t{numreads}\t{data_yield}\t{n50}\t{median_length}\t{median_pid}")
+    let bampath = Path::new(&bamp);
+    let bamname = bampath.file_name().unwrap().to_str().unwrap();
+    let checksum = hash_file(bampath, checksums::Algorithm::MD5);
+    println!("bam\tnumber_of_reads\tyield\tn50\tmedian_length\tmedian_pid\thistogram_length\thistogram_pid\tbam_path\tbam_checksum");
+    println!(
+        "{bamname}\t{numreads}\t{data_yield}\t{n50}\t{median_length}\t{median_pid}\t{bamp}\t{checksum}"
+    )
 }
 
 #[cfg(test)]
@@ -84,4 +91,9 @@ fn init() {
 fn verify_app() {
     use clap::CommandFactory;
     Cli::command().debug_assert()
+}
+
+#[test]
+fn extract() {
+    extract_metrics("~/test-data/test.bam".to_string(), 0.01, 8)
 }
