5f905044cfcec9c0544d4d8bae22ffd4086014bf
wdecoster <decosterwouter@gmail.com>
2022-10-04 12:50:42 +0200

    make histogram functions print, also phaseblocks

--- src/histograms.rs
+++ src/histograms.rs
@@ -1,16 +1,15 @@
 use histo_fp::Histogram;
 use std::cmp::Ordering;
 
-pub fn make_histogram_lengths(array: &[u64]) -> Histogram {
+pub fn make_histogram_lengths(array: &[u64]) {
     let mut histogram = Histogram::with_buckets(100, Some(2));
     for value in array.iter() {
         histogram.add(*value as f64);
     }
-
-    histogram
+    println!("\n\nHistogram for lengths\n{}", histogram);
 }
 
-pub fn make_histogram_identities(array: &[f32]) -> Histogram {
+pub fn make_histogram_identities(array: &[f32]) {
     let bins = 100.0
         - array
             .iter()
@@ -20,6 +19,13 @@ pub fn make_histogram_identities(array: &[f32]) -> Histogram {
     for value in array.iter() {
         histogram.add(f64::from(*value));
     }
+    println!("\n\nHistogram for identities\n{}", histogram);
+}
 
-    histogram
+pub fn make_histogram_phaseblocks(array: &[i64]) {
+    let mut histogram = Histogram::with_buckets(100, Some(2));
+    for value in array.iter() {
+        histogram.add(*value as f64);
+    }
+    println!("\n\nHistogram for phaseblocks\n{}", histogram);
 }
