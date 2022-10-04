f5eda7904b139fbdc593b58e77de32f9ce5d2204
wdecoster <decosterwouter@gmail.com>
2022-10-04 12:51:43 +0200

    use reference to tids

--- src/karyotype.rs
+++ src/karyotype.rs
@@ -1,14 +1,14 @@
 use rust_htslib::bam::{self, Read};
 use std::collections::HashMap;
 
-pub fn make_karyotype(tids: Vec<i32>, bamp: String) {
+pub fn make_karyotype(tids: &Vec<i32>, bamp: String) {
     let bam = bam::Reader::from_path(bamp).unwrap();
     let header = bam::Header::from_template(bam.header());
     let head_view = bam::HeaderView::from_header(&header);
 
     let mut tidcount: HashMap<i32, usize> = HashMap::new();
     for tid in tids {
-        *tidcount.entry(tid).or_default() += 1;
+        *tidcount.entry(*tid).or_default() += 1;
     }
     let mut chroms = vec![];
     let mut norm_count = vec![];
