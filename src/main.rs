273573cadc298546e39e6a77040737f620e1ac28
wdecoster <decosterwouter@gmail.com>
2023-06-01 10:53:00 +0200

    support reading from stdin
    apparently code was already in place in extract_from_bam.rs,
    but the is_file checker didn't like it

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,10 @@ struct Cli {
     spliced: bool,
 }
 
-fn is_file(pathname: &str) -> Result<(), String> {
+pub fn is_file(pathname: &str) -> Result<(), String> {
+    if pathname == "-" {
+        return Ok(());
+    }
     let path = PathBuf::from(pathname);
     if path.is_file() {
         Ok(())
